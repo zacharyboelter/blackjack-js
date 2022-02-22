5a17203b27605d1dbc92be377cb8c1d5c63ea25f
Zachary Boelter <zboelter@gmail.com>
2022-02-22 13:38:17 -0600

    isAlive and blackJack vars

--- app/app.js
+++ app/app.js
@@ -2,13 +2,20 @@
 
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
-
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
 
 if (sum <= 20) {
-    console.log('Do you want another card? 🙂')
+    console.log('Do you want another card? 🙂');
 } else if (sum === 21) {
-    console.log('Blackjack! You win! 🥳')
+    console.log('Blackjack! You win! 🥳');
+    hasBlackJack = true;
 } else {
-    console.log('Bust... You lose shithead. 😭')
+    console.log('Bust... You lose, loser. 😭');
+    isAlive = false;
 } 
+
+console.log(hasBlackJack);
+console.log(isAlive);

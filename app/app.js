95ba097f6632413539d0721ae86ead6437fc97e7
Zachary Boelter <zboelter@gmail.com>
2022-02-22 15:32:44 -0600

    startGame to renderGame

--- app/app.js
+++ app/app.js
@@ -1,17 +1,25 @@
 
-
+//Initial variables
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = '';
-
+// Query selector variables
 let messageEl = document.querySelector('#message-el');
 let sumEl = document.querySelector('#sum-el');
 let cardsEl = document.querySelector('#cards-el');
 
+
+// Start game for onclick start game button
 function startGame() {
+    renderGame();
+}
+
+
+// Main game logic
+function renderGame() {
     if (sum <= 20) {
         message = 'Do you want another card?';
     } else if (sum === 21) {
@@ -24,18 +32,19 @@ function startGame() {
 
     console.log(hasBlackJack);
     console.log(isAlive);
-    
+    // Update html 
     cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`;
     sumEl.textContent = `Sum:  ${sum}`;
     messageEl.textContent = `You have ${sum}, ${message}`;
     console.log(`You have ${sum}, ${message}`);
 }
 
+
+// new card button with updating document
 function newCard() {
     console.log('Drawing a new card from the deck!')
 
     let thirdCard = Math.floor(Math.random() * 11) + 2;
-    sum = sum + thirdCard;
-    
-    startGame();
+    sum +=thirdCard;
+    renderGame();
 }

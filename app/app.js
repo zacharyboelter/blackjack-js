23e3149aea57f321e369c547ccb60df95aaa903c
Zachary Boelter <zboelter@gmail.com>
2022-02-22 12:55:29 -0600

    initial logic

--- app/app.js
+++ app/app.js
@@ -4,3 +4,13 @@ let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
 
 let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log('Do you want another card?')
+} else if (sum === 21) {
+    console.log('Blackjack! You win!')
+} else if (sum > 21) {
+    console.log('Bust... You lose shithead.')
+} else {
+    console.log('Error')
+}

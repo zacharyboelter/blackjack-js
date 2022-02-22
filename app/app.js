9b580b4ad1b6491c8776cbf3d1b41a2ba7b2d146
Zachary Boelter <zboelter@gmail.com>
2022-02-22 14:27:52 -0600

    startGame function working and more styling

--- app/app.js
+++ app/app.js
@@ -7,16 +7,20 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = '';
 
-if (sum <= 20) {
-    message = 'Do you want another card? 🙂';
-} else if (sum === 21) {
-    message = 'Blackjack! You win! 🥳';
-    hasBlackJack = true;
-} else {
-    message = 'Bust... You lose, loser. 😭';
-    isAlive = false;
-} 
 
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+
+function startGame() {
+    if (sum <= 20) {
+        message = 'Do you want another card? 🙂';
+    } else if (sum === 21) {
+        message = 'Blackjack! You win! 🥳';
+        hasBlackJack = true;
+    } else {
+        message = 'Bust... You lose, loser. 😭';
+        isAlive = false;
+    }
+
+    console.log(hasBlackJack);
+    console.log(isAlive);
+    console.log(`You have ${sum}, ${message}`);
+}

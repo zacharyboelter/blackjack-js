c1185e806c1a54da9e6ff66ff59d2da15db7049c
Zachary Boelter <zboelter@gmail.com>
2022-02-22 15:27:08 -0600

    new card button with function and styling

--- app/app.js
+++ app/app.js
@@ -7,21 +7,35 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = '';
 
-let messageEl = document.getElementById('message-el');
+let messageEl = document.querySelector('#message-el');
+let sumEl = document.querySelector('#sum-el');
+let cardsEl = document.querySelector('#cards-el');
 
 function startGame() {
     if (sum <= 20) {
-        message = 'Do you want another card? 🙂';
+        message = 'Do you want another card?';
     } else if (sum === 21) {
-        message = 'Blackjack! You win! 🥳';
+        message = 'Blackjack! You win!';
         hasBlackJack = true;
     } else {
-        message = 'Bust... You lose, loser. 😭';
+        message = 'Bust... You lose, loser.';
         isAlive = false;
     }
 
     console.log(hasBlackJack);
     console.log(isAlive);
-    messageEl.textContent = `You have ${sum}, ${message}`
+    
+    cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`;
+    sumEl.textContent = `Sum:  ${sum}`;
+    messageEl.textContent = `You have ${sum}, ${message}`;
     console.log(`You have ${sum}, ${message}`);
+}
+
+function newCard() {
+    console.log('Drawing a new card from the deck!')
+
+    let thirdCard = Math.floor(Math.random() * 11) + 2;
+    sum = sum + thirdCard;
+    
+    startGame();
 }
